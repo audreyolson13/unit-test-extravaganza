66a18d93c4ef94220c2966b44d49fd70eb8fd94e
Marcos Martins <115571777+marcos-u-martins@users.noreply.github.com>
2025-05-29 11:12:44 -0600

    feat: add 'use client'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
+'use client'
 import React from "react";
 import styles from "./page.module.css";
-import ExampleComponent from "../example1/example1";
+// import ExampleComponent from "../example2/example2";
+import TaskManager from "@/example2/example2";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       {/* Switch out this component with other example components */}
-      <ExampleComponent users={[]} />
+      <TaskManager initialTasks={[
+  { id: 1, title: "Task 1", completed: false },
+  { id: 2, title: "Task 2", completed: true },
+]} />
     </div>
   );
 }
