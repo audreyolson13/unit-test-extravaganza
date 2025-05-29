fc1f5e58b3235585740613bf3236a1e3700e6d51
Marcos Martins <115571777+marcos-u-martins@users.noreply.github.com>
2025-05-29 11:14:46 -0600

    Revert "feat: add 'use client'"
    This reverts commit 66a18d93c4ef94220c2966b44d49fd70eb8fd94e.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
-'use client'
 import React from "react";
 import styles from "./page.module.css";
-// import ExampleComponent from "../example2/example2";
-import TaskManager from "@/example2/example2";
+import ExampleComponent from "../example1/example1";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       {/* Switch out this component with other example components */}
-      <TaskManager initialTasks={[
-  { id: 1, title: "Task 1", completed: false },
-  { id: 2, title: "Task 2", completed: true },
-]} />
+      <ExampleComponent users={[]} />
     </div>
   );
 }
